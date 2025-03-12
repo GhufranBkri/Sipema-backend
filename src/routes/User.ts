3b29005f81ecb76ce6786428c2ea6a702d8af0d4
Muhammad Ghufran <muhammadghufran@MacBook-Air-Muhammad-2.local>
2025-03-12 23:47:13 +0700

    fix: add checkRole for user management

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,32 +1,46 @@
-
-import { Hono } from "hono"
-import * as UserController from "$controllers/rest/UserController"
+import { Hono } from "hono";
+import * as UserController from "$controllers/rest/UserController";
 import * as AuthMiddleware from "$middlewares/authMiddleware";
 import * as UserValidation from "$validations/UserValidation";
+import * as authMiddleware from "$middlewares/authMiddleware";
 
 const UserRoutes = new Hono();
 
+UserRoutes.get(
+  "/",
+  AuthMiddleware.checkJwt,
+  authMiddleware.checkRole(["ADMIN"]),
+  UserController.getAll
+);
 
-UserRoutes.get("/",
-    AuthMiddleware.checkJwt, UserController.getAll
-)
+UserRoutes.get(
+  "/:id",
+  AuthMiddleware.checkJwt,
+  authMiddleware.checkRole(["ADMIN"]),
+  UserController.getById
+);
 
+UserRoutes.post(
+  "/",
+  UserValidation.validateUserRegisterDTO,
+  AuthMiddleware.checkJwt,
+  authMiddleware.checkRole(["ADMIN"]),
+  UserController.create
+);
 
-UserRoutes.get("/:id",
-    AuthMiddleware.checkJwt, UserController.getById
-)
+UserRoutes.put(
+  "/:id",
+  AuthMiddleware.checkJwt,
+  authMiddleware.checkRole(["ADMIN"]),
+  UserController.update
+);
 
+UserRoutes.delete(
+  "/",
+  UserValidation.validationDeletedUsers,
+  AuthMiddleware.checkJwt,
+  authMiddleware.checkRole(["ADMIN"]),
+  UserController.deleteByIds
+);
 
-UserRoutes.post("/",
-    UserValidation.validateUserRegisterDTO, AuthMiddleware.checkJwt, UserController.create
-)
-
-UserRoutes.put("/:id",
-    AuthMiddleware.checkJwt, UserController.update
-)
-
-UserRoutes.delete("/",
-    UserValidation.validationDeletedUsers, AuthMiddleware.checkJwt, UserController.deleteByIds
-)
-
-export default UserRoutes
+export default UserRoutes;
